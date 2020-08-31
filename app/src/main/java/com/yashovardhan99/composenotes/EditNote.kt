3bc38791e39a47ab3336ec5871ec132e73007274
yashovardhan99 <yashovardhandhanania@gmail.com>
2020-09-01 00:05:04 +0530

    Added scroll to text editor

--- app/src/main/java/com/yashovardhan99/composenotes/EditNote.kt
+++ app/src/main/java/com/yashovardhan99/composenotes/EditNote.kt
@@ -2,14 +2,17 @@ package com.yashovardhan99.composenotes
 
 import androidx.compose.foundation.BaseTextField
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.ScrollableColumn
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.savedinstancestate.savedInstanceState
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.viewModel
@@ -20,14 +23,16 @@ fun NoteEditor() {
     val notesViewModel = viewModel(NotesViewModel::class.java)
     val note = notesViewModel.selectedNote ?: notesViewModel.createNote()
     var value by savedInstanceState(saver = TextFieldValue.Saver) { TextFieldValue(note.text) }
-    ScrollableColumn() {
-        BaseTextField(
-            value = value, onValueChange = {
-                value = it
-                notesViewModel.updateNote(note, it.text)
-            },
-            modifier = Modifier.fillMaxSize()
-                .padding(20.dp)
-        )
-    }
+    val scrollState = rememberScrollState()
+    BaseTextField(
+        keyboardType = KeyboardType.Text,
+        imeAction = ImeAction.NoAction,
+        value = value, onValueChange = {
+            value = it
+            notesViewModel.updateNote(note, it.text)
+        },
+        modifier = Modifier.fillMaxSize()
+            .padding(20.dp)
+            .verticalScroll(scrollState)
+    )
 }
