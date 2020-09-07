4d81368429804dd42dc90089f04a93e07a5a2b51
yashovardhan99 <yashovardhandhanania@gmail.com>
2020-09-07 22:53:19 +0530

    Migrated to support FTS

--- app/src/main/java/com/yashovardhan99/composenotes/Note.kt
+++ app/src/main/java/com/yashovardhan99/composenotes/Note.kt
@@ -1,6 +1,7 @@
 package com.yashovardhan99.composenotes
 
 import androidx.room.Entity
+import androidx.room.Fts4
 import androidx.room.PrimaryKey
 import androidx.room.TypeConverter
 import java.util.*
@@ -11,6 +12,12 @@ data class Note(
     var text: String,
     val created: Date,
     var lastModified: Date
+)
+
+@Fts4(contentEntity = Note::class)
+@Entity(tableName = "note_search")
+data class NoteFts(
+    var text: String
 )
 
 class Converters {
