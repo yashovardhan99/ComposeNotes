8bd5a5288669e82ce2614cb739daf2dfee9c7a2b
yashovardhan99 <yashovardhandhanania@gmail.com>
2020-09-09 00:31:28 +0530

    Updated tests

--- app/src/androidTest/java/com/yashovardhan99/composenotes/NoteDaoTest.kt
+++ app/src/androidTest/java/com/yashovardhan99/composenotes/NoteDaoTest.kt
@@ -7,7 +7,8 @@ import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.take
 import kotlinx.coroutines.runBlocking
 import org.junit.After
-import org.junit.Assert
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -26,22 +27,50 @@ class NoteDaoTest {
         Timber.d("Created database")
     }
 
-    // TODO: 08/09/20 : Tests for inserting notes
-    // TODO: 08/09/20 : Tests for deleting notes
-    // TODO: 08/09/20 : Tests for getting all notes
-    // TODO: 08/09/20 : Tests for getting specific notes
-    // TODO: 08/09/20 : Tests for searching notes
-
+    /**
+     * Test to verify that the database is empty when initialized
+     */
     @Test
-    fun sampleTest() {
+    fun verifyEmpty() {
         Timber.d("Starting test")
         runBlocking {
             notesDao.loadAllNotes().take(1).collect {
                 Timber.d("Received list = $it")
-                Assert.assertTrue(it.isEmpty())
+                assertTrue(it.isEmpty())
             }
         }
     }
+
+    /**
+     * Test to verify notes are being inserted properly
+     */
+    @Test
+    fun verifyInserted() {
+        runBlocking {
+            notesDao.insertNote(TestUtil.note1)
+            notesDao.insertNote(TestUtil.note2)
+            notesDao.loadAllNotes().take(1).collect {
+                assertEquals(it.size, 2)
+                assertEquals(it[0], TestUtil.note1)
+                assertEquals(it[1], TestUtil.note2)
+            }
+        }
+    }
+
+    @Test
+    fun verifyDeleted() {
+        runBlocking {
+            notesDao.insertNote(TestUtil.note1, TestUtil.note2)
+            notesDao.deleteNote(TestUtil.note1)
+            notesDao.loadAllNotes().take(1).collect {
+                assertEquals(1, it.size)
+                assertEquals(TestUtil.note2, it[0])
+            }
+        }
+    }
+    // TODO: 08/09/20 : Tests for getting all notes
+    // TODO: 08/09/20 : Tests for getting specific notes
+    // TODO: 08/09/20 : Tests for searching notes
 
     @After
     fun closeDb() {
---
+++ app/src/androidTest/java/com/yashovardhan99/composenotes/TestUtil.kt
@@ -0,0 +1,14 @@
+package com.yashovardhan99.composenotes
+
+import java.util.*
+
+object TestUtil {
+    val timestamp = Calendar.getInstance()
+        .apply {
+            set(Calendar.YEAR, 2020)
+            set(Calendar.MONTH, Calendar.SEPTEMBER)
+            set(Calendar.DAY_OF_MONTH, 14)
+        }.time
+    val note1 = Note(id = 1, text = "Note 1", created = timestamp, lastModified = timestamp)
+    val note2 = Note(id = 2, text = "Note 2", created = timestamp, lastModified = timestamp)
+}
