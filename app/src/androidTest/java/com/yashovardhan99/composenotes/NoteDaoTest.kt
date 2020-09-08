fecdd05049cc5b02242b1790748025b119644c56
yashovardhan99 <yashovardhandhanania@gmail.com>
2020-09-08 23:37:02 +0530

    Fixed tests

--- app/src/androidTest/java/com/yashovardhan99/composenotes/NoteDaoTest.kt
+++ app/src/androidTest/java/com/yashovardhan99/composenotes/NoteDaoTest.kt
@@ -1,14 +1,19 @@
 package com.yashovardhan99.composenotes
 
 import androidx.room.Room
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.take
 import kotlinx.coroutines.runBlocking
 import org.junit.After
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
+import timber.log.Timber
 
+@RunWith(AndroidJUnit4::class)
 class NoteDaoTest {
     private lateinit var database: NoteDatabase
     private lateinit var notesDao: NoteDao
@@ -18,9 +23,7 @@ class NoteDaoTest {
         val context = InstrumentationRegistry.getInstrumentation().targetContext
         database = Room.inMemoryDatabaseBuilder(context, NoteDatabase::class.java).build()
         notesDao = database.noteDao()
-        runBlocking {
-            database.noteDao().insertNote()
-        }
+        Timber.d("Created database")
     }
 
     // TODO: 08/09/20 : Tests for inserting notes
@@ -31,15 +34,18 @@ class NoteDaoTest {
 
     @Test
     fun sampleTest() {
+        Timber.d("Starting test")
         runBlocking {
-            notesDao.loadAllNotes().collect {
-                Assert.assertArrayEquals(it.toTypedArray(), arrayOf())
+            notesDao.loadAllNotes().take(1).collect {
+                Timber.d("Received list = $it")
+                Assert.assertTrue(it.isEmpty())
             }
         }
     }
 
     @After
     fun closeDb() {
+        Timber.d("Closing db")
         database.close()
     }
 
