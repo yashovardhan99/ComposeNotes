3abc174d6dd1980919b29902752998303fa7e91a
yashovardhan99 <yashovardhandhanania@gmail.com>
2020-09-15 17:07:28 +0530

    Added last modified to current time for deleted notes

--- app/src/main/java/com/yashovardhan99/composenotes/NotesViewModel.kt
+++ app/src/main/java/com/yashovardhan99/composenotes/NotesViewModel.kt
@@ -103,6 +103,7 @@ class NotesViewModel @ViewModelInject constructor(
     }
 
     fun undoDelete(note: Note) {
+        note.lastModified = Date()
         viewModelScope.launch {
             repository.insertNote(note)
         }
